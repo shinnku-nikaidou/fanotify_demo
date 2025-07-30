aae60f63db807c4b77ef246ae4a2d0502a4ca9a6
shinnku <nikaidou@shinnku.com>
2025-07-30 11:38:39 +0000

    Update fanotify mark to use current directory file descriptor

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ fn main() -> nix::Result<()> {
         EventFFlags::O_CLOEXEC | EventFFlags::O_RDONLY,
     )?;
 
+    // 打开当前目录作为文件描述符
+    let cwd = std::fs::File::open(".").map_err(|e| nix::errno::Errno::from_raw(e.raw_os_error().unwrap_or(0)))?;
     fan.mark(
         MarkFlags::FAN_MARK_ADD,
         MaskFlags::FAN_ATTRIB,
-        nix::libc::AT_FDCWD,
+        &cwd,
         Some(Path::new("/home")),
     )?;
 
