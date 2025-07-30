9bc22d67600894a0d7928e6974c3a84a2d71ee35
Shinnku <nikaidou@shinnku.com>
2025-07-30 19:24:50 +0800

    Implement fanotify functionality to monitor file attribute changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use nix::sys::fanotify::{EventFFlags, Fanotify, InitFlags, MarkFlags, MaskFlags};
+use std::{fs, os::unix::io::AsRawFd, path::Path};
+
+fn main() -> nix::Result<()> {
+    let fan = Fanotify::init(
+        InitFlags::empty(),
+        EventFFlags::O_CLOEXEC | EventFFlags::O_RDONLY,
+    )?;
+
+    fan.mark(
+        MarkFlags::FAN_MARK_ADD,
+        MaskFlags::FAN_ATTRIB,
+        nix::libc::AT_FDCWD,
+        Some(Path::new("/home")),
+    )?;
+
+    loop {
+        for ev in fan.read_events()? {
+            if !ev.mask().contains(MaskFlags::FAN_ATTRIB) {
+                continue;
+            }
+            if let Some(fd) = ev.fd() {
+                let link = format!("/proc/self/fd/{}", fd.as_raw_fd());
+                if let Ok(path) = fs::read_link(&link) {
+                    println!("[ATTRIB] pid={} path={}", ev.pid(), path.display());
+                }
+            }
+        }
+    }
 }
